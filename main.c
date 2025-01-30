6883cfa6aff46f1d032b0346e4426de013ec4703
David <dzakaran14@gmail.com>
2025-01-30 22:28:52 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -12,9 +12,9 @@
 #define MIN_GAP 100
 #define MAX_GAP 250
 #define PIPE_SPEED 4
-#define CEILING_HEIGHT 1  // Высота потолка
-#define CEILING_WIDTH 600  // Ширина потолка
-#define GROUND_HEIGHT 50   // Высота пола
+#define CEILING_HEIGHT 1
+#define CEILING_WIDTH 600
+#define GROUND_HEIGHT 50
 
 typedef struct {
     int x, height;
@@ -34,7 +34,7 @@ void handle_events(bool *running, Bird *bird, bool *game_over, bool *restart_gam
         } else if (event.type == SDL_KEYDOWN) {
             if (event.key.keysym.sym == SDLK_SPACE) {
                 if (*game_over) {
-                    *restart_game = true; // Устанавливаем флаг перезапуска
+                    *restart_game = true;
                 }
                 bird->velocity = JUMP_STRENGTH;
             }
@@ -46,31 +46,26 @@ void update_bird(Bird *bird) {
     bird->velocity += GRAVITY;
     bird->y += bird->velocity;
 
-    // Проверяем столкновение с потолком
     if (bird->y < CEILING_HEIGHT) {
         bird->y = CEILING_HEIGHT;
         bird->velocity = 0;
     }
 
-    // Проверяем столкновение с полом
     if (bird->y > SCREEN_HEIGHT - GROUND_HEIGHT - 20) {
-        bird->y = SCREEN_HEIGHT - GROUND_HEIGHT - 20; // Устанавливаем птичку на пол
+        bird->y = SCREEN_HEIGHT - GROUND_HEIGHT - 20;
         bird->velocity = 0;
     }
 }
 
 void update_pipes(Pipe *top_pipe, Pipe *bottom_pipe, int *score) {
-    // Обновление позиции труб
     top_pipe->x -= PIPE_SPEED;
     bottom_pipe->x -= PIPE_SPEED;
 
-    // Генерация новых труб, если старые ушли за экран
     if (top_pipe->x + PIPE_WIDTH < 0) {
-        int gap = rand() % (MAX_GAP - MIN_GAP) + MIN_GAP; // Рандомный зазор между трубами
-        top_pipe->height = rand() % (SCREEN_HEIGHT - gap); // Высота верхней трубы
-        bottom_pipe->height = top_pipe->height + gap; // Нижняя труба отодвигается на зазор
+        int gap = rand() % (MAX_GAP - MIN_GAP) + MIN_GAP;
+        top_pipe->height = rand() % (SCREEN_HEIGHT - gap);
+        bottom_pipe->height = top_pipe->height + gap;
 
-        // Устанавливаем новые позиции для труб
         top_pipe->x = SCREEN_WIDTH;
         bottom_pipe->x = SCREEN_WIDTH;
 
@@ -78,7 +73,6 @@ void update_pipes(Pipe *top_pipe, Pipe *bottom_pipe, int *score) {
         bottom_pipe->passed = false;
     }
 
-    // Увеличение счета, если птичка прошла через препятствия
     if (!top_pipe->passed && top_pipe->x < 100) {
         top_pipe->passed = true;
         (*score)++;
@@ -98,13 +92,11 @@ void render(SDL_Renderer *renderer, Bird *bird, Pipe *top_pipe, Pipe *bottom_pip
     SDL_SetRenderDrawColor(renderer, 135, 206, 235, 255);
     SDL_RenderClear(renderer);
 
-    // Отображаем потолок выше экрана
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);  // Красный цвет для потолка
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_Rect ceiling_rect = {(SCREEN_WIDTH - CEILING_WIDTH) / 2, -CEILING_HEIGHT, CEILING_WIDTH, CEILING_HEIGHT};
     SDL_RenderFillRect(renderer, &ceiling_rect);
 
-    // Отображаем пол
-    SDL_SetRenderDrawColor(renderer, 139, 69, 19, 255);  // Коричневый цвет для пола
+    SDL_SetRenderDrawColor(renderer, 139, 69, 19, 255);
     SDL_Rect ground_rect = {0, SCREEN_HEIGHT - GROUND_HEIGHT, SCREEN_WIDTH, GROUND_HEIGHT};
     SDL_RenderFillRect(renderer, &ground_rect);
 
@@ -114,15 +106,12 @@ void render(SDL_Renderer *renderer, Bird *bird, Pipe *top_pipe, Pipe *bottom_pip
 
     SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
     
-    // Отображаем верхнюю трубу
     SDL_Rect top_pipe_rect = { top_pipe->x, 0, PIPE_WIDTH, top_pipe->height };
     SDL_RenderFillRect(renderer, &top_pipe_rect);
     
-    // Отображаем нижнюю трубу
     SDL_Rect bottom_pipe_rect = { bottom_pipe->x, bottom_pipe->height, PIPE_WIDTH, SCREEN_HEIGHT - bottom_pipe->height - GROUND_HEIGHT };
     SDL_RenderFillRect(renderer, &bottom_pipe_rect);
 
-    // Отображение счета
     char scoreText[20];
     sprintf(scoreText, "Score: %d", score);
     SDL_Surface *surface = TTF_RenderText_Solid(font, scoreText, (SDL_Color){255, 255, 255, 255});
@@ -132,7 +121,6 @@ void render(SDL_Renderer *renderer, Bird *bird, Pipe *top_pipe, Pipe *bottom_pip
     SDL_FreeSurface(surface);
     SDL_DestroyTexture(texture);
 
-    // Если игра окончена, отображаем сообщение о конце игры
     if (game_over) {
         SDL_Surface *game_over_surface = TTF_RenderText_Solid(font, "Game Over! Press SPACE to restart.", (SDL_Color){255, 0, 0, 255});
         SDL_Texture *game_over_texture = SDL_CreateTextureFromSurface(renderer, game_over_surface);
@@ -186,7 +174,7 @@ int main() {
         if (game_over && restart_game) {
             reset_game(&bird, &top_pipe, &bottom_pipe, &score);
             game_over = false;
-            restart_game = false; // Сбрасываем флаг перезапуска
+            restart_game = false;
         }
 
         if (!game_over) {
